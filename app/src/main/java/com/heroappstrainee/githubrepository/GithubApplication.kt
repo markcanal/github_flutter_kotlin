e075fef67e98b303fbf6fc475fa4a1257e895cdd
Mark Jerald Canal <markjeraldc@gmail.com>
2022-10-10 17:08:02 +0800

    added: timber dependencies

--- app/src/main/java/com/heroappstrainee/githubrepository/GithubApplication.kt
+++ app/src/main/java/com/heroappstrainee/githubrepository/GithubApplication.kt
@@ -2,10 +2,12 @@ package com.heroappstrainee.githubrepository
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
 class GithubApplication : Application() {
     override fun onCreate() {
         super.onCreate()
+        Timber.plant(Timber.DebugTree())
     }
 }
